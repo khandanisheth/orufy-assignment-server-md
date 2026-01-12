78b1cdbf1b81cc2ca36c3e39fa0d7461afd38592
Mohammad Danish <139689386+khandanisheth@users.noreply.github.com>
2026-01-12 13:45:09 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -21,4 +21,7 @@ mongoose.connect(process.env.MONGO_URI)
     .catch((err) => console.log(err));
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
-app.listen(5000, () => console.log("Server running on port 5000"));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
