d82e1b23cfbfe243a4de47c684ea1fb4199b1c62
Mohammad Danish <139689386+khandanisheth@users.noreply.github.com>
2026-01-12 13:46:17 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -10,6 +10,9 @@ const app = express();
 app.use(express.json());
 dotenv.config();
 app.use(cors());
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
 
 app.use("/uploads", express.static(path.resolve("uploads")));
@@ -23,5 +26,6 @@ app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
 
 
